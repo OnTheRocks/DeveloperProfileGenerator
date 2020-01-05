61885892df68b7e100e6d1c5dd29c7299fb25579
Nathan Huber <Nathan_Huber@yahoo.com>
2020-01-04 21:26:16 -0600

    Corrected the PDF conversion issue.  Tweaked some alignment issues.  Everything seems to be working now.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
+const pdf = require('html-pdf');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+// -------------- Prompting user for Github username and a color ---------------
 
 function promptUser() {
   return inquirer.prompt([
@@ -23,12 +24,10 @@ function promptUser() {
 
 promptUser()
 
+// --------------------- Setting API URL and Setting colors variables for result HTML ----
+
 .then(function(answers) {
-
   const queryUrl = `https://api.github.com/users/${answers.github}`;
-  // const queryUrl = `https://api.github.com/users/${answers.github}/starred`;
-  // const queryUrl = `https://api.github.com/users/${answers.github}/starred{${answers.github}`;
-
   const htmlColors = {
     Green: {
       backGround: "#bae0b8", 
@@ -48,30 +47,16 @@ promptUser()
     },
   };
 
-
-
-
-
-
-  
-
+  // -------------------- Pulling Github Info back --------------------------
 
   axios.get(queryUrl)
   .then(function(res) {
-    console.log(res.data);
-    console.log(res.data.name);
-    console.log(res.data.avatar_url);    
-    console.log(res.data.html_url);
-    console.log(answers.color);
-    console.log(htmlColors);
-    
-    // console.log(html);
-
   
-
+  // -------------------- Generating result.html -------------------------
+    
     function generateHTML() {
-      console.log()
-      console.log(htmlColors);
+      console.log(res.data.location)
+
       return `
       <!DOCTYPE html>
       <html lang="en">
@@ -106,34 +91,33 @@ promptUser()
        }
       
       .jumbotron img {
-               float: left;
-               width: 250px;
-               height: 250px;
-               border-radius: 50%;
-               object-fit: cover;
-               margin-top: -80px;
-               border: 4px solid black;
-               box-shadow: rgba(0, 0, 0, .5) 5px 1px 20px 4px;
-               /* text-align: center; */
-               }
+        float: left;
+        width: 250px;
+        height: 250px;
+        border-radius: 50%;
+        object-fit: cover;
+        margin-top: -80px;
+        border: 4px solid black;
+        box-shadow: rgba(0, 0, 0, .5) 5px 1px 20px 4px;
+        /* text-align: center; */
+        }
 
       .container {
         width: 100%;
-         text-align: left;
-         padding: 20px 0 0 0;
+        text-align: left;
+        padding: 20px 0 0 0;
       }
 
       .link {
         display: inline-block;
-         margin: 5px 10px 0px 10px;
+        margin: 5px 10px 0px 10px;
       }
       
       .card {
-      margin: 15px;
-      border: thin ${htmlColors[answers.color].borderColor} solid;
-      box-shadow: ${htmlColors[answers.color].borderColor} 1px 1px 5px 1px;
-      background-color: ${htmlColors[answers.color].backGround};
-      
+        margin: 15px;
+        border: thin ${htmlColors[answers.color].borderColor} solid;
+        box-shadow: ${htmlColors[answers.color].borderColor} 1px 1px 5px 1px;
+        background-color: ${htmlColors[answers.color].backGround};      
       }
       
       #mainHi {
@@ -142,52 +126,52 @@ promptUser()
       }
       
       .card-title {
-      text-align: center;
+        text-align: center;
       } 
       
       a, a:hover {
         text-decoration: none;
         color: inherit;
       }
+
+      .nameBlock {
+        margin-left: 80px;
+        margin-right: 11px;
+      }
         
       </style>
       
-      <body>
-       
+      <body>       
         <div class="container-lg col-md-8 offset-md-2">
         <div class="jumbotron col-md-12 offset-md-0">
-          <img src="${[res.data.avatar_url]}">
-          
-          <h1 id mainHi class="display-4 col-md-10 offset-md-4">Hi!</h1>
-          <h1 class="display-4 col-md-10 offset-md-4">My Name is ${[res.data.name]}!</h1>
+          <img src="${[res.data.avatar_url]}">          
+          <div class = "nameBlock">
+          <h1 id mainHi class="display-4 col-md-10 offset-md-3">Hi!</h1>
+          <h1 class="display-4 col-md-10 offset-md-3">My Name is ${[res.data.name]}!</h1>
+          </div>
           <br>
           <br>      
           <p class="lead">Currently @ ${[res.data.company]} </p>
           <p class="lead">${[res.data.bio]} </p>
-
           <div class = "container">
             <div class = "link">
               <p class = "lead"><a href = "https://www.google.com/maps/place/${[res.data.location]}" title = "https://www.google.com/maps/place/${[res.data.location]}"><i class="fas fa-map-pin"></i> ${[res.data.location]}</a></p>
             </div>
             <div class = "link">
-            <p class = "lead"><a href = "${[res.data.html_url]}" title = "${[res.data.html_url]}"><i class="fab fa-github-square"></i> GitHub</a></p>
+              <p class = "lead"><a href = "${[res.data.html_url]}" title = "${[res.data.html_url]}"><i class="fab fa-github-square"></i> GitHub</a></p>
             </div>
             <div class = "link">
-            <p class = "lead"><a href = "${[res.data.blog]}" title = "${[res.data.blog]}"><i class="fas fa-rss-square"></i> Blog</a></p>
+              <p class = "lead"><a href = "${[res.data.blog]}" title = "${[res.data.blog]}"><i class="fas fa-rss-square"></i> Blog</a></p>
             </div>
-          </div>
-
-          
-          <hr class="my-4">
-      
-          <div class="row justify-content-lg-center ">
-      
-          <div class="card card-title" style="width: 20rem;">
-            <div class="card-title card-body ">
-              <h5>Public Repositories</h5>
-              <h5>${[res.data.public_repos]}</h5>
+          </div>          
+        <hr class="my-4">      
+          <div class="row justify-content-lg-center ">      
+            <div class="card card-title" style="width: 20rem;">
+              <div class="card-title card-body ">
+                <h5>Public Repositories</h5>
+                <h5>${[res.data.public_repos]}</h5>
+              </div>
             </div>
-          </div>
           <div class="card card-title" style="width: 20rem;">
             <div class="card-title card-body">
               <h5>Followers</h5>
@@ -196,8 +180,8 @@ promptUser()
           </div>
           <div class="card card-title" style="width: 20rem;">
             <div class="card-title card-body">
-              <h5>GitHub Stars</h5>
-              <h5>${[res.data.followers]}</h5>
+              <h5>Public Gists</h5>
+              <h5>${[res.data.public_gists]}</h5>
             </div>
           </div>
           <div class="card card-title" style="width: 20rem;">
@@ -209,23 +193,21 @@ promptUser()
           </div>
           </div>
         </div>
-      </div>
-      
-      
+      </div>     
       </body>
       </html>`;
     }
 
-    const html = generateHTML();  
-  return writeFileAsync("result.html", html);
-})
-.then(function() {
+  const html = generateHTML();  
+
+// ----------------------------- Generates Results.html and Results.pdf -----------------------------------
+
+  var options = { format: 'Letter',"renderDelay": 3000, };
+  fs.writeFileSync("result.html", html);
+  pdf.create(html,options).toFile("result.pdf", function(err, res) {
+    if (err) return console.log(err);
+    console.log(res);
+  })
   console.log("Successfully wrote to result.html");
-
-})
-.catch(function(err) {
-  console.log(err);
-});
-
-
+  })
 })
